497f67ebe1f5db6741ced14d00d2749721b5b4ea
PixeLarm12 <lucasrdomingues-lrd@hotmail.com>
2024-03-23 14:14:49 -0300

    Added Header

---
+++ resources/js/Components/DefaultHeader.vue
@@ -0,0 +1,36 @@
+<template>
+    <header class="w-screen min-h-[5vh] bg-light-black flex items-center">
+        <nav class="w-full h-full flex justify-around items-center gap-6 px-12">
+            <div class="lg:w-5/12 md:w-2/12 md:flex hidden h-full text-center text-white text-3xl font-bold uppercase">
+                Manyminds
+            </div>
+
+            <div class="lg:w-7/12 md:w-10/12 w-full h-full text-center flex justify-end">
+                <div class="lg:w-4/12 md:w-6/12 w-full flex flex-row justify-between">
+                    <router-link class="text-white text-xl font-semibold uppercase " to="/">Products</router-link>
+                    <router-link class="text-white text-xl font-semibold uppercase " to="/">Orders</router-link>
+                    <button class="text-white text-xl font-semibold uppercase " @click="logout()">Exit</button>
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    methods: {
+        async logout() {
+            await axios.get("/api/logout")
+                .then(response => {
+                    if(response) {
+                        localStorage.removeItem("jwt_token")
+                        this.$router.push('/login')
+                    }
+                })
+                .catch(error => console.log(error));
+        },
+    }
+}
+</script>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,5 +1,22 @@
 <template>
+    <default-header></default-header>
+    
     <div class="flex flex-col justify-center items-center">
         <h1>Home Page - Project Example</h1>
     </div>
 </template>
+
+<script>
+import DefaultHeader from '../Components/DefaultHeader.vue';
+
+export default {
+    beforeMount() {
+        if(! localStorage.getItem('jwt_token')) {
+            this.$router.push("/login");
+        }
+    },
+    components: {
+        DefaultHeader,
+    }
+}
+</script>
