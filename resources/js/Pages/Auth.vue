a28238e71d98e8d12d51e51c4da870be3693f40c
PixeLarm12 <lucasrdomingues-lrd@hotmail.com>
2024-03-23 14:14:45 -0300

    updated responsivity

--- resources/js/Pages/Auth.vue
+++ resources/js/Pages/Auth.vue
@@ -1,23 +1,23 @@
 <template>
-    <div class="w-screen min-h-[100vh] flex flex-col justify-center items-center bg-blue-900">
-        <div class="w-4/12 min-h-[40vh] bg-gray-100 flex flex-col justify-center items-center rounded-lg shadow-lg py-10">
-            <h1 class="text-2xl text-black font-semibold uppercase py-4">Login</h1>
+    <div class="w-screen min-h-[100vh] flex flex-col justify-center items-center bg-light-black">
+        <div class="xl:w-4/12 w-11/12 min-h-[40vh] bg-gray-100 flex flex-col justify-center items-center rounded-lg shadow-lg py-10">
+            <h1 class="xl:text-6xl text-4xl text-black font-semibold uppercase py-4">Login</h1>
             <span class="text-red-600 text-lg self-center w-10/12 py-5">{{ errors.auth }}</span>
             <form @submit.prevent="submitForm()" method="POST" class="w-full h-full px-4 grid grid-cols-12 gap-6 place-items-center place-content-center">
-                <div class="col-span-12 w-8/12">
-                    <label for="email" class="text-black text-2xl font-semibold">E-mail</label><br>
+                <div class="col-span-12 xl:w-8/12 w-10/12">
+                    <label for="email" class="text-black xl:text-2xl text-xl font-semibold">E-mail</label><br>
                     <input type="email" id="email" v-model="email" name="email" placeholder="Digite o seu email" class="w-full h-10 px-5 rounded-md border border-black border-opacity-80" required>
                     <span class="text-red-600 text-lg">{{ errors.email }}</span>
                 </div>
 
-                <div class="col-span-12 w-8/12">
-                    <label for="password" class="text-black text-2xl font-semibold">Senha</label><br>
+                <div class="col-span-12 xl:w-8/12 w-10/12">
+                    <label for="password" class="text-black xl:text-2xl text-xl font-semibold">Password</label><br>
                     <input type="password" id="password" v-model="password" name="password" placeholder="Digite a sua senha" class="w-full h-10 px-5 rounded-md border border-black border-opacity-80" required>
                     <span class="text-red-600 text-lg">{{ errors.password }}</span>
                 </div>
 
-                <div class="col-span-12 w-8/12 flex justify-center items-center">
-                    <button type="submit" name="submitButton" id="submitButton" class="w-6/12 h-14 bg-blue-900 text-white text-xl font-semibold rounded-md hover:scale-105 duration-200">Entrar</button>
+                <div class="col-span-12 xl:w-8/12 w-10/12 flex justify-center items-center">
+                    <button type="submit" name="submitButton" id="submitButton" class="button-primary">Submit</button>
                 </div>
             </form>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ export default {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-black': '#121212',
+      }
+    },
   },
   plugins: [],
 }
