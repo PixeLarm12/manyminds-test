72d2511ca4131d5219b8bd646fc78eb71d64be02
PixeLarm12 <lucasrdomingues-lrd@hotmail.com>
2024-03-23 14:17:12 -0300

    Updated header layout

--- resources/js/Components/DefaultHeader.vue
+++ resources/js/Components/DefaultHeader.vue
@@ -2,14 +2,14 @@
     <header class="w-screen min-h-[5vh] bg-light-black flex items-center">
         <nav class="w-full h-full flex justify-around items-center gap-6 px-12">
             <div class="lg:w-5/12 md:w-2/12 md:flex hidden h-full text-center text-white text-3xl font-bold uppercase">
-                Manyminds
+                <router-link to="/">manyminds</router-link>
             </div>
 
             <div class="lg:w-7/12 md:w-10/12 w-full h-full text-center flex justify-end">
                 <div class="lg:w-4/12 md:w-6/12 w-full flex flex-row justify-between">
-                    <router-link class="text-white text-xl font-semibold uppercase " to="/">Products</router-link>
-                    <router-link class="text-white text-xl font-semibold uppercase " to="/">Orders</router-link>
-                    <button class="text-white text-xl font-semibold uppercase " @click="logout()">Exit</button>
+                    <router-link class="nav-item" to="/">Products</router-link>
+                    <router-link class="nav-item" to="/">Orders</router-link>
+                    <button class="nav-item" @click="logout()">Exit</button>
                 </div>
             </div>
         </nav>
