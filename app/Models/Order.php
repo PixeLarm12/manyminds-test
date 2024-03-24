2486dd7797aaf3afb16fe70cd1c062b99cb2aac7
PixeLarm12 <lucasrdomingues-lrd@hotmail.com>
2024-03-24 14:34:27 -0300

    Create Order migration and model | Fixed suppliers table

---
+++ app/Models/Order.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    public const ORDER_STATUS_ACTIVE = 'active';
+    public const ORDER_STATUS_FINISHED = 'finished';
+
+    protected $table = 'orders';
+
+    protected $fillable = [
+        'supplier_id',
+        'status',
+        'observation'
+    ];
+
+    public static function getStatusByKey($key) 
+    {
+        $data = self::getStatus();
+
+        foreach($data as $item) {
+            if($item['key'] == $key) {
+                return $item;
+            }
+        }
+    }
+
+    public static function getStatus() 
+    {
+        return [
+            [
+                'key' => self::ORDER_STATUS_ACTIVE,
+                'value' => 'Active',
+            ],
+            [
+                'key' => self::ORDER_STATUS_FINISHED,
+                'value' => 'Finished',
+            ],
+        ];
+    }
+}
